6215a60a227c27cb5865f1464beb4a22be4d27f0
Tyler Cox <tyler.a.cox@berkeley.edu>
2021-11-19 08:43:54 -0800

    Documentation updates

--- galaxies_widget.py
+++ galaxies_widget.py
@@ -7,8 +7,12 @@ import subprocess
 import os, os.path, sys
 from astropy.io import ascii
 
+from scipy.optimize import curve_fit
+
+from scipy.ndimage import gaussian_filter1d
+
 _ERASESTR = ""
-_CACHE_BASEDIR = os.path.join(os.getenv("HOME"), ".galdyncourse", "cache")
+_CACHE_BASEDIR = os.path.join(os.getenv("HOME"), "cache")
 _CACHE_VIZIER_DIR = os.path.join(_CACHE_BASEDIR, "vizier")
 _ADIBEKYAN_VIZIER_NAME = "J/A+A/545/A32"
 
@@ -106,3 +110,125 @@ def get_sn_xhdist(element, plotrange=[-3, 1], bins=31):
     )
     b = (b[1:] + b[:-1]) / 2
     return c, b
+
+
+Zsolar = 0.014
+
+
+def fgas(t, mode="", tau=11e9):
+    """
+    Gas fraction
+    """
+    if mode == "constant":
+        C = 0.7 / 13e9
+        fraction = 1 - C * t
+        return fraction
+
+    else:
+        C = 0.7 / (tau - tau * np.exp(-13e9 / tau))
+        fraction = 1 - C * (tau - tau * np.exp(-t / tau))
+        return fraction
+
+
+def calc_metallicity(t, mode="constant", tau=11e9, z_yield=0.5):
+    """
+    """
+    return -z_yield * np.log(fgas(t, mode=mode, tau=tau))
+
+
+def metallicity_distribution(Z, z_yield=0.01):
+    """
+    """
+    Z *= Zsolar
+    return Z / z_yield * np.exp(-Z / z_yield)
+
+
+def fit_closed(logZ, z_yield=0.01):
+    """
+    """
+    dN = metallicity_distribution(10 ** logZ, z_yield=z_yield)
+    return dN / np.trapz(dN, logZ)
+
+
+def calc_metallicity_leaky(t, mode="constant", tau=11e9, z_yield=0.01, eta=3):
+    """
+    """
+    return -z_yield / (1 + eta) * np.log(fgas(t, mode=mode, tau=tau))
+
+
+def metallicity_distribution_leaky(Z, z_yield=0.01, eta=3, r=0.2):
+    """
+    """
+    Z *= Zsolar
+    pp = z_yield / (1 + eta - r)
+    return Z / pp * np.exp(-Z / pp)
+
+
+def fit_leaky(logZ, z_yield=0.01, eta=3):
+    """
+    """
+    dN = metallicity_distribution_leaky(10 ** logZ, z_yield=z_yield, eta=eta)
+    return dN / np.trapz(dN, logZ)
+
+
+def calc_metallicity_accreting(t, mode="constant", tau=11e9, z_yield=0.01, eta=3):
+    """
+    """
+    return calc_metallicity_accreting_leaky(
+        t, mode=mode, tau=tau, z_yield=z_yield, eta=eta, r=0
+    )
+
+
+def calc_metallicity_accreting_leaky(
+    t, mode="constant", tau=11e9, z_yield=0.01, eta=3, r=0.4
+):
+    """
+    """
+    return (
+        z_yield
+        / (1 + eta - r)
+        * (1 - np.exp((1 + eta - r) / (1 - r) * (1 - 1 / fgas(t, mode=mode, tau=tau))))
+    )
+
+
+def dNdZ_accreting(Z):
+    """
+    """
+
+
+def metallicity_distribution_accreting(Z, z_yield=0.01):
+    """
+    """
+    return Z / z_yield * np.exp(-Z / z_yield)
+
+
+def metallicity_distribution_accreting_leaky(Z, z_yield=0.01, eta=3, r=0.5):
+    """
+    """
+    Z *= Zsolar
+    pp = z_yield / (1 + eta - r)
+    m = Z / (pp - Z)
+    m[Z > pp] = 0
+    return m
+
+
+def fit_accreting(logZ, z_yield=0.01, eta=3):
+    """
+    """
+    dN = metallicity_distribution_accreting_leaky(
+        10 ** logZ, z_yield=z_yield, eta=eta, r=0
+    )
+    k = gaussian_filter1d(np.abs(dN), 3)
+    dk = k / np.trapz(k, logZ)
+    return dk
+
+
+def fit_accreting_leaky(logZ, z_yield=0.01, eta=3, r=0.5):
+    """
+    """
+    dN = metallicity_distribution_accreting_leaky(
+        10 ** logZ, z_yield=z_yield, eta=eta, r=r
+    )
+    k = gaussian_filter1d(np.abs(dN), 3)
+    dk = k / np.trapz(k, logZ)
+    return dk
